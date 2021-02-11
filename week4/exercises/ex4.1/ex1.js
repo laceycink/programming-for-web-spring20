4bb2a6466271424fed30c832577401a3ae09f726
laceycink <60250900+laceycink@users.noreply.github.com>
2021-02-11 08:15:00 -0700

    Update ex1.js

--- week4/exercises/ex4.1/ex1.js
+++ week4/exercises/ex4.1/ex1.js
@@ -3,7 +3,7 @@ const myEmptyDiv = document.querySelector("#myEmptyDiv");
 // put an h1 inside
 //create new element
 const heading = document.createElement('h1');
-heading.innerHTML = 'Hello class! It is a snowy day!';
+heading.innerHTML = 'Llamas are a relative of the camel';
 heading.style.cursor = 'pointer';
 heading.addEventListener('click', handleHeadingClick);
 myEmptyDiv.appendChild(heading);
